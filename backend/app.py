4d4dc9d4f08947ef14e04c510b86f429edfccca9
Pedro Dias <pedro.dias@aluno.cefetmg.br>
2025-12-03 18:34:11 -0300

    feat[docker]: creating dockerfile

--- backend/app.py
+++ backend/app.py
@@ -665,4 +665,4 @@ def get_target_pdb(target_name):
         return jsonify({'status': 'error', 'message': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
